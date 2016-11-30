14f2a23ea353e8d0a47511260598fef6356d82c2
Onur Cankaya <oecankaya@gmail.com>
2016-11-30 15:44:23 +0100

    Add test suite for list component

---
+++ tests/components/Todo.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import $ from 'jQuery';
+import TestUtils from 'react-addons-test-utils';
+
+import Todo from 'Todo';
+
+
+describe('Todo', () => {
+  it('should exist', () => {
+    expect(Todo).toExist();
+  });
+});
